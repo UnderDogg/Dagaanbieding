60fa3d674d3cd554dfc13e2d58d7a62fe769b792
MRVDH <mrvdhoven@gmail.com>
2015-05-22 18:23:59 +0200

    Updated the history page

--- js/controllers/HistoryController.js
+++ js/controllers/HistoryController.js
@@ -2,7 +2,13 @@ angular.module("Dagaanbieding")
 
 .controller("historyController", ['$scope', '$location', 'ajaxService', function($scope,  $location, ajaxService) {
     'use strict';
-    
+
+    // Initialises the MaterializeCSS select fields.
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+
+
     ajaxService.get('./api/getDailyInfo/getAll', function(data) {
             // Convert date
             data.forEach(function(item) {
