65a9f8307c6418c1b7ae9c32f0980e99a44ab335
MRVDH <mrvdhoven@gmail.com>
2015-05-22 19:04:41 +0200

    filter functions first test

--- js/controllers/HistoryController.js
+++ js/controllers/HistoryController.js
@@ -1,17 +1,33 @@
-angular.module("Dagaanbieding")
+var historyApp = angular.module("Dagaanbieding")
 
-.controller("historyController", ['$scope', '$location', 'ajaxService', function($scope,  $location, ajaxService) {
+historyApp.controller("historyController", ['$scope', '$location', 'ajaxService', function($scope,  $location, ajaxService) {
     'use strict';
+    $scope.allData = [];
+    $scope.content = [];
+
+    $scope.filterData = function () {
+        var month = $scope.selectedMonth;
+        var year = $scope.selectedYear;
+
+        if (month !== "" && year != "") {
+
+        }
+    };
 
     ajaxService.get('./api/getDailyInfo/getAll', function (data) {
-            // Convert date
-            data.forEach(function (item) {
-                console.log("old date", item.date);
-                item.date = new Date(item.date);
-                console.info("new date", item.date);
-            });
-            
-            // Fill content object
-            $scope.content = data;
+        var currentDate = new Date();
+
+        data.forEach(function (item) {
+            item.date = new Date(item.date);
+            console.log(currentDate.getFullYear());
+            console.log(item.date.getFullYear());
+            console.log(currentDate.getMonth());
+            console.log(item.date.getMonth());
+            if (currentDate.getFullYear() === item.date.getFullYear() && currentDate.getMonth() === item.date.getMonth()) {
+                $scope.content.push(item);
+            }
+        });
+
+        $scope.allData = data;
     });
 }]);
