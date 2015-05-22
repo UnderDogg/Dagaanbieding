ba4175126f9973f8d7d0441adc29893172453974
MRVDH <mrvdhoven@gmail.com>
2015-05-22 18:27:07 +0200

    Wrong html file lel

--- js/controllers/HistoryController.js
+++ js/controllers/HistoryController.js
@@ -3,12 +3,6 @@ angular.module("Dagaanbieding")
 .controller("historyController", ['$scope', '$location', 'ajaxService', function($scope,  $location, ajaxService) {
     'use strict';
 
-    // Initialises the MaterializeCSS select fields.
-    $(document).ready(function() {
-        $('select').material_select();
-    });
-
-
     ajaxService.get('./api/getDailyInfo/getAll', function(data) {
             // Convert date
             data.forEach(function(item) {
