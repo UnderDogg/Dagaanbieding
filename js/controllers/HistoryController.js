da883e12acbd75330d4c0a3c2f18eeaf87867496
MRVDH <mrvdhoven@gmail.com>
2015-05-22 18:39:48 +0200

    Check date status

--- js/controllers/HistoryController.js
+++ js/controllers/HistoryController.js
@@ -3,13 +3,15 @@ angular.module("Dagaanbieding")
 .controller("historyController", ['$scope', '$location', 'ajaxService', function($scope,  $location, ajaxService) {
     'use strict';
 
-    ajaxService.get('./api/getDailyInfo/getAll', function(data) {
+    ajaxService.get('./api/getDailyInfo/getAll', function (data) {
             // Convert date
-            data.forEach(function(item) {
+            data.forEach(function (item) {
+                console.log("old date", item.date);
                 item.date = new Date(item.date);
+                console.info("new date", item.date);
             });
             
-            // Fille content object
+            // Fill content object
             $scope.content = data;
     });
 }]);
