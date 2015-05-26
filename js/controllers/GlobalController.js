eded12c0db68dd264a6191bd06c7779e95825881
Els <edenengelse@gmail.com>
2015-05-26 10:37:53 +0200

    added button collapse menu mobile

--- js/controllers/GlobalController.js
+++ js/controllers/GlobalController.js
@@ -2,7 +2,13 @@ angular.module("Dagaanbieding")
 
 .controller("globalController", ['$scope', '$location', function($scope,  $location) {
     'use strict';
-    
+
+    $( document ).ready(function(){
+
+        $(".button-collapse").sideNav();
+
+    });
+
     $scope.isActive = function (viewLocation) {
         return $location.path() === viewLocation;
     };
