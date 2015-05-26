05330701a9d4bace6d8fcd146aa1667147a2a972
Els <edenengelse@gmail.com>
2015-05-26 10:56:17 +0200

    added settings to side-nav

--- js/controllers/GlobalController.js
+++ js/controllers/GlobalController.js
@@ -5,7 +5,11 @@ angular.module("Dagaanbieding")
 
     $( document ).ready(function(){
 
-        $(".button-collapse").sideNav();
+        $(".button-collapse").sideNav({
+            menuWidth: 250, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        });
 
     });
 
